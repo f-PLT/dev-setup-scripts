982fcc74893b3bc1e020ef9c473484dff67103e0
Firstname Lastname <pelletier.f@gmail.com>
2019-09-20 19:55:48 -0400

    modified install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,13 @@ sudo dpkg -i chef-workstation_0.8.7-1_amd64.deb
 # Setup chef-home folder
 mkdir -p ~/chef-home/.chef ~/chef-home/cookbooks
 
+# Run cookbook
+cd ~/chef-home
+sudo chef-client -z -o dev-setup
+
+# Test installation
+#sudo chef-client --local-mode 'recipe[test-dev-home]'
+cd ~
+
 # Cleanup
 rm -rf chef-workstation_0.8.7-1_amd64.deb 
