a09f1b5d511ae57a43e6ce29f3dbe7798bf2a3d6
f_PLT <pelletier.f@gmail.com>
2019-10-03 00:52:20 -0400

    webtools: change Node install + tools

--- recipes/webtools.rb
+++ recipes/webtools.rb
@@ -7,8 +7,26 @@ package 'php7.3-xdebug'
 package 'php-cgi'
 package 'php-sqlite3'
 
-execute "install node via snap store" do
-    command "sudo snap install node --classic --channel=8"
+execute "add Node v10 LTS to package manager" do
+    command "curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
+end
+
+package 'nodejs'
+
+execute "add Yarn public key" do
+    command "curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
+end
+
+execute "add Yarn to package manager" do
+    command "echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
+end
+
+execute "install Yarn" do
+    command "sudo apt-get update && sudo apt-get install yarn"
+end
+
+execute "install angular/cli" do
+    command "sudo npm install -g @angular/cli"
 end
 
 execute "install core-js" do
