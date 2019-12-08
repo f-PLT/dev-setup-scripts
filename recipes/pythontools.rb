a72ca9f759792738b9bb5d861438edcc7b93aff0
Francis Pelletier <pelletier.f@gmail.com>
2019-12-08 06:26:14 +0000

    Python PostGre

--- recipes/pythontools.rb
+++ recipes/pythontools.rb
@@ -3,9 +3,15 @@ package 'python3-pip'
 package 'pylint3'
 package 'idle3'
 package 'virtualenv'
+package 'python-dev'
+package 'libpq-dev'
 
 execute "install pip virtual environment" do
     command "sudo pip3 install virtualenv"
+end
+
+execute "install postgre" do
+    command "sudo pip install psycopg2"
 end
 
 execute "install Pycharm via snap store" do
