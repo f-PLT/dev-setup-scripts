912f54b0a36e52b57a4028892e245f09ff09f38a
f_PLT <pelletier.f@gmail.com>
2021-01-17 14:50:22 -0500

    Add export path for conda init

--- scripts/pythontools_config.sh
+++ scripts/pythontools_config.sh
@@ -28,6 +28,7 @@ if [ ! -d "$HOME/miniconda3" ]; then
 echo 'Fetching and installing miniconda3'
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
     bash ~/miniconda.sh -b -p $HOME/miniconda3
+    export PATH=$HOME/miniconda3/bin:$PATH
     conda init
     rm ~/miniconda.sh
 else
