cf5ae43089ed296c3bb189961186a803a2550819
f_PLT <pelletier.f@gmail.com>
2021-01-18 19:16:45 -0500

    Add missing steps to Docker

--- scripts/dockertools_config.sh
+++ scripts/dockertools_config.sh
@@ -11,6 +11,11 @@ docker -v
 if [ $? != "0" ]
     then
         echo "Installing Docker"
+        sudo apt-get install \
+            apt-transport-https \
+            ca-certificates \
+            gnupg-agent \
+            software-properties-common
         echo 'Adding GPG key'
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -31,11 +36,13 @@ docker-compose -v
 if [ $? != "0" ]
     then
         echo 'Installing docker-compose'
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         echo 'change of permissions for docker-compose'
         sudo chmod +x /usr/local/bin/docker-compose
 
 fi
 echo '*'
 echo '* Docker configuration script - DONE'
+echo '* Please either reboot your machine or execute the following:'
+echo '* $ newgrp docker'
 echo '*'
