d061efe4e71e7faa0d4059420caab6d2069f31d5
f_PLT <pelletier.f@gmail.com>
2021-02-05 08:53:40 -0500

    Separate vimrc configuration

--- scripts/basictools.sh
+++ scripts/basictools.sh
@@ -8,9 +8,6 @@ build-essential \
 software-properties-common \
 vim
 
-echo 'Copying vimrc to home'
-cp files/vimrc ~/.vimrc
-
 echo '*'
 echo '* Basictools configuration script - DONE'
 echo '*'
---
+++ scripts/vimconf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo '*'
+echo '* Vim configuration script - STARTING'
+echo '*'
+vim -V
+if [ $? != "0" ]
+    then
+        sudo apt-get install -y vim
+fi
+echo 'Copying vimrc to home'
+cp files/vimrc ~/.vimrc
+
+echo '*'
+echo '* Bash shell configuration script - DONE'
+echo '* You will need to refresh your bash for it to take effect'
+echo '*'
