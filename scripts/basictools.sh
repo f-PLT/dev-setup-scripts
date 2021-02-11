09e6740a6cf18e33316a6062fd0ea0bc43bd763b
Francis Pelletier <pelletier.f@gmail.com>
2021-02-11 22:03:37 +0000

    Adjust system file watcher limit in basictools

--- scripts/basictools.sh
+++ scripts/basictools.sh
@@ -8,6 +8,9 @@ build-essential \
 software-properties-common \
 vim
 
+echo 'Setting System file watcher'
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
 echo '*'
 echo '* Basictools configuration script - DONE'
 echo '*'
