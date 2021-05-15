caedf6150c98a58db7263c0ab478379d44238f29
f_PLT <pelletier.f@gmail.com>
2021-05-15 19:35:04 -0400

    Change bashconf to append instead of replace

--- scripts/bashconf.sh
+++ scripts/bashconf.sh
@@ -2,8 +2,45 @@
 echo '*'
 echo '* Bash shell configuration script - STARTING'
 echo '*'
-echo 'Copying bashrc file to home'
-cp files/bashrc ~/.bashrc
+echo 'Appending to bashrc file...'
+
+cat << EOF >> ~/.bashrc
+
+# Colors in the terminal
+function parse_git_branch_and_add_brackets {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
+  }
+export LSCOLORS=CxdxxxxxExxxxxExExCxCx
+PS1="\n\e[36m\$(parse_git_branch_and_add_brackets) \e[32;1m\u\e[34;1m@\h\e[0m \e[33;1m[\w]\e[0m\n $ "
+
+# Vim shortcut in command line
+set -o vi
+
+# Giteditor
+export GIT_EDITOR=`which vim`
+
+# Python alias
+alias py='python3'
+alias python='python3'
+alias pip='pip3'
+
+# Safety alias
+alias cp='/bin/cp -i'
+alias mv='/bin/mv -i'
+alias rm='/bin/rm -i'
+
+# keygen alias
+alias keygen='ssh-keygen -t rsa -b 4096'
+
+# History pipe alias
+alias hgrep='history | grep'
+
+# Conda aliases
+alias createEnv='conda create python=3 -n'
+
+# Docker
+alias dcp='docker-compose'
+EOF
 
 echo '*'
 echo '* Bash shell configuration script - DONE'
