884f4d9837d31ae3b50a6a14699d0ab2ec598a9b
f-PLT <pelletier.f@gmail.com>
2025-05-18 16:16:30 -0400

    Add note about Yarn

--- scripts/webtools_config.sh
+++ scripts/webtools_config.sh
@@ -8,10 +8,14 @@ echo 'Configuring NVM'
 echo 'Installing and configuring nvm packages'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
+echo ''
 echo 'The following commands will need to be run in a new terminal'
 echo '$ nvm install stable'
 echo '$ nvm use stable' 
-
+echo ''
+echo 'To install Yarn, run the follwoing command after install NPM with the above commands'
+echo '$ npm install --global yarn'
+echo ''
 echo '*'
 echo '* Web tools configuration script - DONE'
 echo '*'
